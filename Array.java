bc90b3e8584bed1fdebebe7cf5b65f7dc4027223
Souravedas <144267597+Souravedas@users.noreply.github.com>
2023-09-26 23:27:04 +0600

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,13 +1,13 @@
 package Com.Shakal;
 
 import java.util.Scanner;
-
+//sum of two numbers from an array of targeted input and print the index
 public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of array elements : ");
         int n = sc.nextInt();
-        int flag = 0;
+        int flag = 0;//to see the completion of if condition
         int[] arr = new int[n];
         System.out.print("Enter some integer numbers : ");
         for(int i = 0; i <n; i++){
@@ -17,7 +17,7 @@ public class Array {
         int target = sc.nextInt();
         for(int i = 0; i<n-1; i++){
             for(int j = i+1; j<n; j++){
-                if(arr[i] + arr[j] == target){
+                if(arr[i] + arr[j] == target){//to see if any of the two integers are equal to the target or not
                     System.out.println("["+i+","+j+"]");
                     flag = 1;
                     break;
